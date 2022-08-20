26ee8cc5c551762421a8b3bdac2e3cc687cf7491
Andrew Kleonskiy <co95310a@mail.ru>
2022-08-20 23:51:36 +0300

    client add

--- module_socket.py
+++ module_socket.py
@@ -4,3 +4,6 @@ s.bind(('127.0.0.1',8888))
 result = s.recv(1024)
 print('Message:', result.decode('utf-8'))
 s.close()
+#сторона клиента
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(b'test', ('127.0.0.1', 8888))
