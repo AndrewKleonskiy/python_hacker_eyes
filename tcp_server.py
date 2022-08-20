406cdd46d24fed4fe13bf5d3deb43485e8a1ee52
Andrew Kleonskiy <co95310a@mail.ru>
2022-08-21 00:06:40 +0300

    tcp adds

---
+++ tcp_server.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 8888))
+
+while 1:
+    try:
+        client, addr = s.accept()
+    except:
+        s.close()
+        break
+    else:
+        result = client.recv(1024)
+        print('Message:',result.decode('utf-8'))
+
